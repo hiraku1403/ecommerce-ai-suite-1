2445f87951bc71badb9eb1797f9431b77cf002f9
matheus <sartorhenn@gmail.com>
2026-07-24 19:21:12 -0300

    fix: atualizando vercel.json para o padrao serverless e adicionando whitenoise

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-<<<<<<< HEAD
 import os
-=======
-
->>>>>>> eb4204f1c6c1288a10d2cf4546b09401e09be9f8
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,21 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
 
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-t5@o&qs50b4bh3@2_66+u@*^-h5dwsa9su*1(^9h5%cm-s-&pg')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
-=======
-SECRET_KEY = 'django-insecure-t5@o&qs50b4bh3@2_66+u@*^-h5dwsa9su*1(^9h5%cm-s-&pg'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
->>>>>>> eb4204f1c6c1288a10d2cf4546b09401e09be9f8
 
 
 # Application definition
@@ -56,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -63,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 
 ROOT_URLCONF = 'config.urls'
 
@@ -129,18 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-<<<<<<< HEAD
-
-
 STATIC_URL = '/static/'
-=======
-import os
-
-STATIC_URL = 'static/'
->>>>>>> eb4204f1c6c1288a10d2cf4546b09401e09be9f8
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
