29dde2e25fe136dd1f364cf95ce927c65f3ccea9
matheus <sartorhenn@gmail.com>
2026-07-24 19:37:45 -0300

    fix: adicionando auto-migrate no wsgi para o banco temporario da Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# Se estiver rodando na Vercel, executa as migrações automaticamente no banco temporário
+if os.getenv('VERCEL'):
+    try:
+        call_command('migrate', interactive=False)
+    except Exception as e:
+        print(f"Erro ao rodar auto-migrate na Vercel: {e}")
+
+app = application
